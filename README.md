7de8109f85abfe995edcbee7829be0c2fb02857e
James Neal <janeal@mines.edu>
2017-04-12 12:27:12 -0600

    Added new data and updated README a bit.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ncaa-march-madness-predictor
+
+A Matlab tool for predicting the outcome of the 2017 March Madness bracket.
--- main.m
+++ main.m
@@ -1,5 +1,10 @@
 % An prediction tool for the NCAA March Madness 2017 basketball tournament.
 % The tournament already happened, so this is kind of moot.  Oh well.
+%
+% Authors:
+%   Patrick Sobolewski - psobolew@mines.edu
+%   Jeffrey Stephens - jstephen@mines.edu
+%   James Neal - janeal@mines.edu
 % -------------------------------------------------------------------------
 
 % Read in data into Matlab tables below.  Tables have columns with names,
@@ -8,6 +13,7 @@
 
 % detailed results of games played during the season
 season = readtable('RegularSeasonDetailedResults.csv');
+adv_season = readtable('2016-2017 Season Statistics.csv');
 
 % teams competing in the tournament and their associated id
 teams = readtable('Teams.csv');
