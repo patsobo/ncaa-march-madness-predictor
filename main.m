157d87cc980f4894ddb493b9ae7b3383508fad5a
James Neal <janeal@mines.edu>
2017-04-17 13:46:09 -0600

    Wrote notes for the algorithm, rough working model.

--- main.m
+++ main.m
@@ -20,3 +20,26 @@ teams = readtable('Teams.csv');
 
 % the seed ranking of each team for the 2017 NCAA tournament.
 seeds = readtable('TourneySeeds.csv');
+
+
+% Building our algorithm now
+
+% our hand-picked categories
+categories = ['defense', 'ppg', 'reb', 'fg', '3pt', 'ft', 'ftm', '3pm', 'wl', 'oreb', 'to'];
+pmf = [.25, .15, .08, .08, .08, .08, .08, .08, .16, .08, .20];
+
+% these four categories will always be used in the algorithm
+indexes = [1, 2, 9, 11];
+
+% add three random categories to the algorithm
+for i = 1:3
+    random = rand;
+    slice = 1/7;
+end
+
+% run through each category and determine a winner, add to a total score
+l_score = 0;
+r_score = 0;
+
+% determine winner
+winner = 'UNC';
